ca79a3fdf9cefe86419443d0fd933768ceb88325
Aryan Bola <aaryann5002@gmail.com>
2025-10-21 17:37:37 +0530

    fixed the navbar, now to make new

--- src/components/NavBar2.tsx
+++ src/components/NavBar2.tsx
@@ -8,7 +8,8 @@ export default function NavBar2() {
 
   return (
     <div className="h-screen w-full bg-white rounded-xl border flex-center">
-      <div className="flex-center gap-8 rounded-3xl bg-black p-2 px-4 overflow-hidden">
+      <div className="flex-center gap-8 rounded-3xl bg-black p-2 px-8 overflow-hidden">
+        {/* 1 */}
         <div className={`relative p-1 rounded-b-full transition-all duration-200 ease-in-out ${selected=="home" ? "bg-white -translate-y-3" : "bg-black"}`}>
           <div className={`absolute -left-4 w-4 h-4 ${selected=="home" ? "bg-white" : "bg-black"}`}>
             <div className="w-full h-full bg-black rounded-tr-full"/>
@@ -18,7 +19,7 @@ export default function NavBar2() {
           </div>
           <p onClick={()=>setSelected("home")} className={`cursor-pointer rounded-full p-2 ${selected == "home" ? "bg-lime-500 text-black" : "bg-black text-white hover:bg-neutral-700"}`}><GoHome size={40}/></p>
         </div>
-        
+        {/* 2 */}
         <div className={`relative p-1 rounded-b-full transition-all duration-200 ease-in-out ${selected=="clock" ? "bg-white -translate-y-3" : "bg-black"}`}>
           <div className={`absolute -left-4 w-4 h-4 ${selected=="clock" ? "bg-white" : "bg-black"}`}>
             <div className="w-full h-full bg-black rounded-tr-full"/>
@@ -28,7 +29,7 @@ export default function NavBar2() {
           </div>
           <p onClick={()=>setSelected("clock")} className={`cursor-pointer rounded-full p-2 ${selected == "clock" ? "bg-lime-500 text-black" : "bg-black text-white hover:bg-neutral-700"}`}><GoClock size={40}/></p>
         </div>
-
+        {/* 3 */}
         <div className={`relative p-1 rounded-b-full transition-all duration-200 ease-in-out ${selected=="search" ? "bg-white -translate-y-3" : "bg-black"}`}>
           <div className={`absolute -left-4 w-4 h-4 ${selected=="search" ? "bg-white" : "bg-black"}`}>
             <div className="w-full h-full bg-black rounded-tr-full"/>
@@ -38,7 +39,7 @@ export default function NavBar2() {
           </div>
           <p onClick={()=>setSelected("search")} className={`cursor-pointer rounded-full p-2 ${selected == "search" ? "bg-lime-500 text-black" : "bg-black text-white hover:bg-neutral-700"}`}><GoSearch size={40}/></p>
         </div>
-
+        {/* 4 */}
         <div className={`relative p-1 rounded-b-full transition-all duration-200 ease-in-out ${selected=="person" ? "bg-white -translate-y-3" : "bg-black"}`}>
           <div className={`absolute -left-4 w-4 h-4 ${selected=="person" ? "bg-white" : "bg-black"}`}>
             <div className="w-full h-full bg-black rounded-tr-full"/>
